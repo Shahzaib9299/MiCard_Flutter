8358fc3465990436571788c30aba280cc672f06f
Shahzaib9299 <shahzaib12824@gmail.com>
2025-07-25 13:02:40 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal[600],
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('My Card'),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.all(30.0)),
+              Center(
+                child: CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('assets/images/shahzaib.jpg'),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Khawaja Afaiz',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Fluter Developer",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                // color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.amber,
+                  ),
+                  title: Text(
+                    "+92 344 1512824",
+                    style: TextStyle(color: Colors.teal, fontSize: 18.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.amber,
+                  ),
+                  title: Text(
+                    "shahzaib12824@gmail.com",
+                    style: TextStyle(
+                        color: Colors.teal[600],
+                        fontFamily: "SourceSanPro",
+                        fontSize: 18.0),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
